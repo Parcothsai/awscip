e0d9512cf2b1526f177142ddcb6ddf9105780812
ParcoThsai <thsaip@gmail.com>
2023-04-14 13:38:40 +0200

    [UPDATE] change_ip.sh

--- scripts/change_ip.sh
+++ scripts/change_ip.sh
@@ -4,6 +4,31 @@ domain_file="/scripts/data/domains.list"
 zone_id=$(printenv AWS_ZONE_ID)
 dir_log="/scripts/logs"
 
+EchoLog()
+{
+        msg="$1"
+        echo "[$(date '+%d-%m-%Y-%H-%M')]: $msg"
+        echo "[$(date '+%d-%m-%Y-%H-%M')]: $msg" >> "$dir_log/domains.log"
+}
+ValidateIp()
+{
+        ip_to_check=$1
+        message=$2
+        if expr "$ip_to_check" : '[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*$' >/dev/null; then
+          IFS=.
+          set $ip_to_check
+          for quad in 1 2 3 4; do
+            if eval [ \$$quad -gt 255 ]; then
+              EchoLog "Could not get current IP address: $newip , exit 1"
+              exit 1
+            fi
+          done
+          EchoLog "$message $ip_to_check is valid"
+        else
+          EchoLog "Could not get current IP address: $ip_to_check , exit 1"
+          exit 1
+        fi
+}
 CreateOrUpdateDnsZone() {
         zone_id="$2"
         domain=$1
@@ -11,32 +36,16 @@ CreateOrUpdateDnsZone() {
         # The AWS id for the zone containing the record, obtained by logging into aws route53
         zoneid=$zone_id
         newip=$(curl -s ifconfig.me)
-        if expr ! "$newip" : "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$"
-        then
-                echo "Could not get current IP address: $newip" >> "$dir_log/domains.log"
-                exit 1
-        fi
-        echo "$newip"
+        ValidateIp "$newip" "New IP"
         # Get the IP address record that AWS currently has, using AWS's DNS server
         oldip=$(dig +short "$hostname")
-        # oldip=$(dig +short "$hostname" @"$nameserver")
-        echo "$oldip" >> "$dir_log/domains.log"
-        # if  ! expr "$oldip" :  "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$"
-        if  [ ! "$oldip" = "$newip" ]
-        then
-                echo "$(date) -> $domain : Could not get old ip address" >> "$dir_log/domains.log"
-                # exit 1
-        fi
-
+        ValidateIp "$oldip" "Old IP"
         # Bail if everything is already up to date
-        if [ "$newip" = "$oldip" ]
-        then
-                echo "No change need" >> "$dir_log/domains.log"
-                echo "$(date) $domain is up to date" >> "$dir_log/domains.log"
+        if [ "$newip" = "$oldip" ]; then
+                EchoLog "$domain is up to date"
                 return 1
         fi
-        echo "$domain : change $oldip to $newip  on $(date)"
-        # aws route53 client requires the info written to a JSON file
+        EchoLog "$domain : change $oldip to $newip"
         tmp=$(mktemp /tmp/dynamic-dns.XXXXXXXX)
         cat > "${tmp}" << EOF
         {
@@ -53,8 +62,7 @@ CreateOrUpdateDnsZone() {
         }
 
 EOF
-
-        echo "Changing IP address of $hostname from $oldip to $newip" >> "$dir_log/domains.log"
+        EchoLog "Changing IP address of $hostname from $oldip to $newip"
         aws route53 change-resource-record-sets --hosted-zone-id "$zoneid" --change-batch "file://$tmp"
 
         rm "$tmp"
@@ -62,14 +70,13 @@ EOF
 
 
 if [ ! -f $domain_file ]; then
-        echo "Please create a file with domains list" >> "$dir_log/domains.log"
+        EchoLog "Please create a file with domains list"
         exit 1
 fi 
 
-echo "-----------LOGFILE-----------" >> "$dir_log/domains.log"
 grep -v '^ *#' < $domain_file | while IFS= read -r domain
 do
-        echo "-----------$domain-----------" >> "$dir_log/domains.log"
+        EchoLog "-----------$domain-----------"
         CreateOrUpdateDnsZone "$domain" "$zone_id"
 done
 
